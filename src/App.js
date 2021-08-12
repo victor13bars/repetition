39138632a2f5e4d20234689320fd0e0c27109e6c
victor13bars <victor_bars_bars95@mail.ru>
2021-08-12 12:38:48 +0300

    Add, sort, search posts.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React, {useState} from 'react'
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React, {useMemo, useState} from 'react'
 import './styles/App.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'JavaScript1', body: "JS - My Favorite languages"},
-        {id: 2, title: 'JavaScript2', body: "JS - My Favorite languages"},
-        {id: 3, title: 'JavaScript3', body: "JS - My Favorite languages"}
+        {id: 1, title: 'aa', body: "zz"},
+        {id: 2, title: 'dd', body: "cc"},
+        {id: 3, title: 'bb', body: "ee"},
+        // {id: 1, title: 'JavaScript1', body: "JS - My Favorite languages"},
+        // {id: 2, title: 'JavaScript2', body: "JS - My Favorite languages"},
+        // {id: 3, title: 'JavaScript3', body: "JS - My Favorite languages"}
     ])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+
+    const sortedPosts = useMemo(() => {
+        console.log("getSortedPosts")
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
     }
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title="Список Pro JS"/>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список Pro JS"/>
         </div>
     );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -12,7 +12,7 @@ const PostItem = (props) => {
                 </div>
             </div>
             <div className="post__btns">
-                <MyButton>DELETE</MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
             </div>
         </div>
     );
