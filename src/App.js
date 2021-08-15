2d2fcc3c041f4553dd71cf1295d7dd9bd66b3195
victor13bars <victor_bars_bars95@mail.ru>
2021-08-15 14:24:36 +0300

    Axios, API, PostService, Loader.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,19 +6,27 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/Mymodal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePost";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aa', body: "zz"},
-        {id: 2, title: 'dd', body: "cc"},
-        {id: 3, title: 'bb', body: "ee"},
-        // {id: 1, title: 'JavaScript1', body: "JS - My Favorite languages"},
-        // {id: 2, title: 'JavaScript2', body: "JS - My Favorite languages"},
-        // {id: 3, title: 'JavaScript3', body: "JS - My Favorite languages"}
-    ])
+    const [posts, setPosts] = useState([])
     const [modal, setModal] = useState(false)
     const [filter, setFilter] = useState({sort: '', query: ''})
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostLoading, setIsPostLoading] = useState(false)
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+    async function fetchPosts() {
+        setIsPostLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostLoading(false)
+    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -38,7 +46,10 @@ function App() {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список Pro JS"/>
+            {isPostLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список Pro JS"/>
+            }
         </div>
     );
 }
---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Loader.module.css'
+
+const Loader = () => {
+    return (
+        <div className={s.loader}>
+
+        </div>
+    );
+};
+
+export default Loader;
